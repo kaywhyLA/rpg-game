3e448fa4ae254b7de41b5caa47b2d6a4459adb64
KHAYLLA HITE <KHAYLLAHOUSTON@YAHOO.COM>
2022-07-30 00:15:49 -0500

    added object test and inventory test to player.test.js

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -5,8 +5,6 @@ const Potion = require('../lib/potion');
 jest.mock('../lib/potion');
 console.log(new Potion());
 
-
-
 test('creates a player objects', () => {
     const player = new Player('Dave');
 
@@ -17,4 +15,23 @@ test('creates a player objects', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test("gets players stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
