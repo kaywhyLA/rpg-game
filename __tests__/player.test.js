035e377bf72d98c21aafbb268b333bfe351250b6
KHAYLLA HITE <KHAYLLAHOUSTON@YAHOO.COM>
2022-07-30 00:07:18 -0500

    added inventory array to player.js, tested and failed out

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -1,5 +1,12 @@
 const Player = require('../lib/player');
 
+const Potion = require('../lib/potion');
+
+jest.mock('../lib/potion');
+console.log(new Potion());
+
+
+
 test('creates a player objects', () => {
     const player = new Player('Dave');
 
@@ -7,4 +14,7 @@ test('creates a player objects', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 });
